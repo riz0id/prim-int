f2abbfe2c023d7b8427d9b862f8938b3b64afb5e
Jake Leach <jacobleach@protonmail.com>
2022-09-24 21:40:01 -0500

    refact: pinned `nixpkgs` use extensions

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -2,20 +2,17 @@
 
 let
   nixpkgs = import ./nixpkgs.nix { };
-
-  overlay = self: super: {
-    haskell = super.haskell // {
-      packages = super.haskell.packages // {
-        "${ghc}" = super.haskell.packages."${ghc}".override (_: {
-          overrides = (new: _: {
-            prim-bool   = new.callPackage pkgs/prim-bool.nix { };
-            prim-compat = new.callPackage pkgs/prim-compat.nix { };
-            prim-int    = new.callCabal2nix "prim-int" ../. { };
-          });
-        });
-      };
-    };
-  };
 in import nixpkgs {
-  overlays = [ overlay ];
+  config.packageOverrides = pkgs: 
+    pkgs.lib.composeManyExtensions [  
+      (import extensions/prim-bool.nix {
+        inherit ghc;
+      })
+      (import extensions/prim-compat.nix {
+        inherit ghc;
+      })
+      (import extensions/prim-int.nix {
+        inherit ghc;
+      })
+    ] pkgs pkgs;
 }
